48520fab24cde0804ff00332ba9137eb9c49e881
Di <Di@DESKTOP-4Q6F3EE>
2023-02-14 21:15:06 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber , secondNumber;
+            string action;
+
+            Console.WriteLine("Input firstnumber : ");
+
+            firstNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input secondnumber : ");
+
+            secondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input action +,-,*,/");
+            
+            action = Console.ReadLine();
+
+            if (action == "+")
+            {
+                double resultadd = firstNumber + secondNumber;
+
+                Console.WriteLine(firstNumber + " + " + secondNumber + " = " + resultadd);
+            }
+            else if (action == "-")
+            {
+                double resultsub = firstNumber - secondNumber;
+
+                Console.WriteLine(firstNumber + " - " + secondNumber + " = " + resultsub);
+            }
+            else if (action == "*")
+            {
+                double resultmult = firstNumber * secondNumber;
+                Console.WriteLine(firstNumber + " * " + secondNumber + " = " + resultmult);
+            }
+            else if (action == "/")
+            {
+                double resultdiv = firstNumber / secondNumber;
+
+                Console.WriteLine(firstNumber + " / " + secondNumber + " = " + resultdiv);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
